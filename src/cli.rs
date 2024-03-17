26b3301a043abf56d1530d06c3ed4ba0b42398e6
markichnich <markichnich@proton.me>
2024-03-17 20:41:35 +0100

    add arg parsing for previously hardcoded variables

---
+++ src/cli.rs
@@ -0,0 +1,45 @@
+use clap::{command, value_parser, Arg, ArgAction, Command, ValueHint};
+
+pub fn new() -> Command {
+    command!()
+        .about("Simply convert images to ASCII art")
+        .arg(
+            Arg::new("input")
+                .help("Input image file")
+                .action(ArgAction::Set)
+                .value_name("INPUT_FILE")
+                .value_parser(value_parser!(std::path::PathBuf))
+                .value_hint(ValueHint::FilePath)
+                .required(true),
+        )
+        .arg(
+            Arg::new("output")
+                .short('o')
+                .long("output")
+                .help("Optional output text file - If omitted, writes to stdout")
+                .action(ArgAction::Set)
+                .value_name("FILE")
+                .value_parser(value_parser!(std::path::PathBuf))
+                .value_hint(ValueHint::FilePath),
+        )
+        .arg(
+            Arg::new("size")
+                .short('s')
+                .long("size")
+                .help("Output size in characters along the larger of the images dimensions")
+                .action(ArgAction::Set)
+                .value_name("INTEGER")
+                .value_parser(value_parser!(u32))
+                .default_value("25"),
+        )
+        .arg(
+            Arg::new("x-stretch")
+                .short('x')
+                .long("x-stretch")
+                .help("Factor to stretch the image by along its horizontal")
+                .action(ArgAction::Set)
+                .value_name("FLOAT")
+                .value_parser(value_parser!(f64))
+                .default_value("2.3"),
+        )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-use std::env;
 use image::GenericImageView;
+use std::{fmt, fs::File, io, path::PathBuf};
+
+mod cli;
 
 const SYMBOLS: &[char] = &[' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
-const PIXEL_SIZE: f64 = 25.0;
-const X_STRETCH: f64 = 2.0;
 
 fn main() -> Result<(), &'static str> {
-    let filename: String;
-    match env::args().into_iter().nth(1) {
-        None => return Err("No filename provided."),
-        Some(arg) => filename = arg,
-    }
+    let matches = cli::new().get_matches();
 
-    let img = image::open(filename).unwrap();
+    let input_file: &PathBuf = matches.get_one("input").unwrap();
+    let x_stretch: f64 = *matches.get_one("x-stretch").unwrap();
+    let size: u32 = *matches.get_one("size").unwrap();
+
+    let img = image::open(input_file).unwrap();
     let (w, h) = img.dimensions();
 
-    let a = u32::max(h, w) as f64 / PIXEL_SIZE;
-    let w = (w as f64 / a * X_STRETCH) as u32;
-    let h = (h as f64 / a) as u32;
+    let scale = size as f64 / u32::max(h, w) as f64;
+    let w = (w as f64 * scale * x_stretch) as u32;
+    let h = (h as f64 * scale) as u32;
 
     let resized = img.resize_exact(w, h, image::imageops::FilterType::Nearest);
 
-    resized.pixels()
+    let data = resized
+        .pixels()
         .map(|(_, _, color)| {
             let sum: u16 = color.0.iter().take(3).map(|&v| v as u16).sum::<u16>();
             let normalized = sum as f64 / (3.0 * 255.0);
@@ -31,9 +32,27 @@ fn main() -> Result<(), &'static str> {
         })
         .collect::<Vec<_>>()
         .chunks(w as usize)
-        .for_each(|chunk| {
-            println!("{}", chunk.iter().collect::<String>())
-        });
+        .fold(
+            String::with_capacity((size as usize).pow(2)),
+            |mut s, line| {
+                fmt::Write::write_str(&mut s, &line.iter().chain(&['\n']).collect::<String>())
+                    .unwrap();
+                s
+            },
+        );
+
+    match matches.get_one::<PathBuf>("output") {
+        None => print!("{data}"),
+        Some(path) => {
+            let mut file = File::create(path).unwrap_or_else(|_| {
+                panic!(
+                    "Failed to open output file: '{}'",
+                    path.to_str().unwrap_or_default()
+                )
+            });
+            io::Write::write(&mut file, data.as_bytes()).unwrap();
+        }
+    }
 
     Ok(())
 }
